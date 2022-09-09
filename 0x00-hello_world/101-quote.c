174f8660b5fdc7ffa91859b84fc1331387cef326
Efobi Victor <efobivictor357@gmail.com>
2022-09-08 18:51:46 -0700

    Advance 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints to standard error
+ *
+ * Return: 1.
+ */
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+	write(1, quote, 59);
+	return (1);
+}
