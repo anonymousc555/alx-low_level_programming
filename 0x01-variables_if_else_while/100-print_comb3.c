ffefa407225de299721a458bd341382c26050164
Efobi Victor <efobivictor357@gmail.com>
2022-09-10 10:04:06 -0700

    Advance Task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints combination of two numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 8; num++)
+	{
+		putchar((num1 % 10) + '0');
+		if (num1 == 8)
+			break;
+	}
+	for (num2 = 1; num2 <= 9; num2++)
+	{
+		putchar((num2 % 10) + '0');
+		if (num2 == 9)
+			break;
+	}
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
