cbea0e3661943db5b31436082d538146774bbad5
Efobi Victor <efobivictor357@gmail.com>
2022-09-12 16:51:02 -0700

    Advance Task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,14 +15,14 @@ int main(void)
 	{
 		for (num2 = 0; num2 <= 99; num2++)
 		{
+			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
-
 			putchar(' ');
-			
-			putchar((num1 % 10) + '0');
+			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
+			if (num1 == 98 && nun2 == 99)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
