aabf1579b234d6df29038d329d4ac941a389ee28
Efobi Victor <efobivictor357@gmail.com>
2022-09-12 16:41:13 -0700

    Advance Task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry point
+ *
+ * Description: print all possible combination of two two digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = 0; num2 <= 99; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			putchar(' ');
+			
+			putchar((num1 % 10);
+			putchar((num2 % 10) + '0');
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
