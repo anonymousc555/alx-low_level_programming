f9b2a7d8c09be0b55c97e860b0a08c6280abaa31
Efobi Victor <efobivictor357@gmail.com>
2022-09-13 16:24:00 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,7 +13,8 @@ void print_alphabet(void)
 
 	char c[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
 
-	for(a = 0; a < (int) sizeof(c); a++)
+	for (a = 0; a < (int) sizeof(c); a++)
 	{
 		_putchar(c[a]);
 	}
+}
