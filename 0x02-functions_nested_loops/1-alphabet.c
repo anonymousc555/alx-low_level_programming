477c8ec275815c174f925c8fe95963df5fd2bade
Efobi Victor <efobivictor357@gmail.com>
2022-09-13 16:16:53 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints alphabet in lower case using cusom header functions
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	char c[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
+
+	for(a = 0; a < (int) sizeof(c); a++)
+	{
+		_putchar(c[a]);
+	}
