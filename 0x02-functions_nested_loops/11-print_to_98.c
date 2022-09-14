6237d5ffef2d783433012a2faf258650efc4f8f0
Efobi Victor <efobivictor357@gmail.com>
2022-09-13 20:11:09 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Function
+ * Description: print to 98
+ * @n: number to begin with
+ * Return: 0 or 1
+ */
+void print_to_98(int n)
+{
+
+while (n < 98)
+{
+	printf("%i, ", n);
+	n++;
+
+}
+while (n < 98)
+{
+	printf("%i, ", n);
+	n--;
+}
+
+printf("98);
+putchar('\n');
+}
