b6b4cd280eabbf210387bfb2629f259e66c6b5f1
Efobi Victor <efobivictor357@gmail.com>
2022-09-25 00:52:39 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,13 @@
 int _putchar(char);
 void swap_int(int *a, int *b);
 void _strlen(char *str);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *s);
+void puts_half(char *str);
+void printf_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
