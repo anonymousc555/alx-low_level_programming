8562887ae62594219b0f449d95584f22f0e0ab9e
Efobi Victor <efobivictor357@gmail.com>
2022-10-04 17:30:32 -0700

    Hot Fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,14 +1,14 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and
- * initializes it with a specific char.
+ * create_array - Creates an array of chars and
+ *                initializes it with a specific char.
  * @size: The size of the array to be initialized.
- * @c: The specific char to initialize the array with.
+ * @c: The specific char to intialize the array with.
  *
- * Return: If the size == 0 or the function fails - NULL.
- * Otherwise - a pointer to the array.
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
  */
 char *create_array(unsigned int size, char c)
 {
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,1 +1,1 @@
-This directory contains contains files for alx Tasks
+**0x0B. C - malloc, free**
